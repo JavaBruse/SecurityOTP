cf92578d2af6545b9c4bac0dc224f761cc47f8ad
FiataliS <FiataliS@gmail.com>
2025-04-10 15:30:32 +0500

    add controller admin func

--- src/main/java/com/javabruse/controllers/AdminController.java
+++ src/main/java/com/javabruse/controllers/AdminController.java
@@ -1,21 +1,42 @@
 package com.javabruse.controllers;
 
-import com.javabruse.service.UserService;
+import com.javabruse.domain.model.OptionsOTP;
+import com.javabruse.domain.model.User;
+import com.javabruse.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
-    private final UserService service;
+    private final AdminService service;
 
     @GetMapping()
     @PreAuthorize("hasRole('ADMIN')")
     public String exampleAdmin() {
         return "Hello, admin!";
+    }
+
+
+    @GetMapping()
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<User> getAllUSer() {
+        return service.getAllUsers();
+    }
+
+    @PostMapping()
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deleteUser(@RequestBody String name) {
+        service.removeUser(name);
+    }
+
+    @PostMapping()
+    @PreAuthorize("hasRole('ADMIN')")
+    public OptionsOTP editOptionsOTP(@RequestBody OptionsOTP optionsOTP) {
+        return service.setOptionsOTP(optionsOTP);
     }
 }
